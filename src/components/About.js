b791816f35b3ad3830ff89b0f4d853640daa3463
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-01-30 13:02:24 -0500

    some links for about

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,8 @@ const About = function About() {
   return (
     <div>
       <p>My name is Tanner Rogalsky.</p>
+      <p>This is my code: <a href="https://github.com/TannerRogalsky">https://github.com/TannerRogalsky</a></p>
+      <p>These are my games: <a href="https://tannerrogalsky.itch.io/">https://tannerrogalsky.itch.io/</a></p>
     </div>
   );
 };
