a7d2e625a6dff495769d2998cae0fe0ce9ad9fcd
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-01-31 09:56:50 -0500

    remove unnecessary div

--- src/components/HtmlLayout.js
+++ src/components/HtmlLayout.js
@@ -11,13 +11,12 @@ export default function HtmlLayout(ComposedComponent) {
         <html>
           <head>
             <meta charSet="utf-8" />
-            <title>Site Title</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Tanner Rogalsky</title>
             <link href="/styles.css" rel="stylesheet" />
           </head>
           <body>
-            <div>
-              <ComposedComponent {...this.props}/>
-            </div>
+            <ComposedComponent {...this.props}/>
           </body>
         </html>
       );
