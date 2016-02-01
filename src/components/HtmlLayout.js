414cfda924c1aea3c97cc8e07c712ec333d90b4d
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-02-01 18:28:26 -0500

    marginally better google analytics inlining

--- src/components/HtmlLayout.js
+++ src/components/HtmlLayout.js
@@ -14,17 +14,7 @@ export default function HtmlLayout(ComposedComponent) {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Tanner Rogalsky</title>
             <link href="/styles.css" rel="stylesheet" />
-            <script dangerouslySetInnerHTML={
-              { __html: `
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-                ga('create', 'UA-30607414-1', 'auto');
-                ga('send', 'pageview');`,
-              }}
-            />
+            <script dangerouslySetInnerHTML={{ __html: require('raw!../client/googleAnalytics.js') }} />
           </head>
           <body>
             <ComposedComponent {...this.props}/>
