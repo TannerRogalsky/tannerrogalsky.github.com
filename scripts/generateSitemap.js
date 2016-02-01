e3facad0743b8dcab11514a94d0c631b6c8e6aa5
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-01-31 19:44:11 -0500

    sitemap generator

---
+++ scripts/generateSitemap.js
@@ -0,0 +1,16 @@
+import sitemap from 'sitemap';
+
+import routes from '../src/routes.js';
+
+const sm = sitemap.createSitemap({
+  hostname: 'http://tannerrogalsky.com',
+  cacheTime: 600000,  // 600 sec cache period
+  urls: Object.keys(routes).map((url) => (
+    {
+      url,
+      lastmodISO: new Date().toISOString(),
+    }
+  )),
+});
+
+export default sm.toString();
