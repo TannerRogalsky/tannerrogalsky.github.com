5133ec6d15636cf0b5b72f6e9e1adb99277a284f
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-02-01 11:07:17 -0500

    optimize occurence plugin is default now

--- webpack.config.production.babel.js
+++ webpack.config.production.babel.js
@@ -3,7 +3,6 @@ import baseConfig from './webpack.config.babel';
 
 const config = Object.create(baseConfig);
 config.plugins.push(...[
-  new webpack.optimize.OccurenceOrderPlugin(),
   new webpack.optimize.DedupePlugin(),
   new webpack.optimize.UglifyJsPlugin({
     compressor: {
