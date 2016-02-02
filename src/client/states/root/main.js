d2deec1bc630fa9af273f921e5b8d4430b4db24a
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-02-02 09:52:09 -0500

    queryable ui structure for main view

--- src/client/states/root/main.js
+++ src/client/states/root/main.js
@@ -8,9 +8,33 @@ const intersectRect = function intersectRect(ax1, ay1, ax2, ay2, bx1, by1, bx2, 
 
 const overlay = document.getElementById('overlay');
 
+const updateHorizontalUIPositions = function updateHorizontalUIPositions(ui, width, height) {
+  ui.arrow.x = width / 2;
+  ui.arrow.y = ui.arrow.h;
+};
+
+const updateVerticalUIPositions = function updateVerticalUIPositions(ui, width, height) {
+  ui.arrow.x = width / 2;
+  ui.arrow.y = ui.arrow.h;
+};
+
+const updateUIPositions = function updateUIPositions(ui, width, height) {
+  if (width > height) {
+    updateHorizontalUIPositions(ui, width, height);
+  } else if (height > width) {
+    updateVerticalUIPositions(ui, width, height);
+  }
+};
+
 const main = {
   enterState() {
     overlay.style.display = 'none';
+
+    const dpr = window.devicePixelRatio;
+    this.ui = {
+      arrow: { x: 0, y: 0 , w: 22 * dpr, h: 22 * dpr },
+    };
+    updateUIPositions(this.ui, this.context.canvas.width, this.context.canvas.height);
 
     this.handleClick = this.handleClick.bind(this);
     this.context.canvas.addEventListener('click', this.handleClick);
@@ -25,8 +49,8 @@ const main = {
     context.fillStyle = DGREEN;
     context.fillRect(0, 0, width, height);
 
-    const arrowSize = 22 * dpr;
-    drawArrow(context, width / 2, arrowSize, arrowSize, arrowSize, Math.PI);
+    const arrow = this.ui.arrow;
+    drawArrow(context, arrow.x, arrow.y, arrow.w, arrow.h, Math.PI);
 
     context.restore();
   },
@@ -35,10 +59,10 @@ const main = {
     const dpr = window.devicePixelRatio;
     const canvas = this.context.canvas;
 
-    const arrowSize = 22 * dpr;
-    const tx = canvas.width / 2 - arrowSize;
-    const ty = 0;
-    const [tw, th] = [arrowSize * 2, arrowSize * 2];
+    const arrow = this.ui.arrow;
+    const tx = arrow.x - arrow.w;
+    const ty = arrow.y - arrow.h;
+    const [tw, th] = [arrow.w * 2, arrow.h * 2];
     const [cx, cy] = [e.clientX * dpr, e.clientY * dpr];
 
     const clickedSwitch = intersectRect(tx, ty, tx + tw, ty + th, cx, cy, cx, cy);
