5ec655a4b9cbba1215c578d5742b2bd896981552
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-10-21 13:54:03 -0400

    link to demo loops from home page

--- src/components/Root.js
+++ src/components/Root.js
@@ -7,7 +7,7 @@ const Root = function Root() {
       <canvas id="bgCanvas" style={{ width: '100vw', height: '100vh', position: 'absolute', left: 0, top: 0 }} />
       <center id="overlay" style={{ position: 'relative' }} >
         <h1>Tanner Rogalsky</h1>
-        <p><a href="/blog/">Blog</a> • <a href="/about/">About</a></p>
+        <p><a href="/blog/">Blog</a> • <a href="/demoloops/">Demoloops</a> • <a href="/about/">About</a></p>
       </center>
       <script src="/root.js" />
     </div>
