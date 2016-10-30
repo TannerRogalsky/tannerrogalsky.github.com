2e8246a8b79c7d3a4897323e4e42fdab365ee158
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-10-30 13:20:13 -0400

    generate blog post script

---
+++ scripts/newBlogPost.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+
+const postName = process.argv[2];
+if (postName == undefined) {
+  throw "Must pass a post name via CLI.";
+}
+
+const leftPad = function leftPad(s) {
+  const str = '0' + s;
+  return str.substring(str.length - 2, str.length);
+}
+
+const blogDataDir = 'src/data/blog/';
+const date = new Date();
+const formattedDate = `${date.getFullYear()}-${leftPad(date.getMonth())}-${leftPad(date.getDate())}`;
+const blogFileName = `${formattedDate}-${postName.toLowerCase().replace(/ /g, '-')}`;
+
+fs.writeFileSync(`${blogDataDir}/${blogFileName}.js`, `
+import reactifyMarkdown from '../../utils/reactify_markdown';
+import markdown from './markdown/${blogFileName}.markdown';
+
+export default {
+  title: '${postName}',
+  date: '${date.toISOString()}',
+  comments: true,
+  categories: [],
+  content: reactifyMarkdown(markdown),
+};
+`);
+
+fs.writeFileSync(`${blogDataDir}/markdown/${blogFileName}.markdown`, '');
