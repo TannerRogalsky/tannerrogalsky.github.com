e2ce5ba6465280a8bd0a6d50731647cb2f552287
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-11-18 13:21:25 -0500

    pure function

--- src/components/HtmlLayout.js
+++ src/components/HtmlLayout.js
@@ -2,22 +2,20 @@ import React from 'react';
 import googleAnalytics from 'raw-loader!../client/googleAnalytics.js';
 
 export default function HtmlLayout(ComposedComponent) {
-  return class Comp extends React.Component {
-    render() {
-      return (
-        <html>
-          <head>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Tanner Rogalsky</title>
-            <link href="/styles.css" rel="stylesheet" />
-            <script dangerouslySetInnerHTML={{ __html: googleAnalytics }} />
-          </head>
-          <body>
-            <ComposedComponent {...this.props} />
-          </body>
-        </html>
-      );
-    }
+  return function Comp(props) {
+    return (
+      <html>
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Tanner Rogalsky</title>
+          <link href="/styles.css" rel="stylesheet" />
+          <script dangerouslySetInnerHTML={{ __html: googleAnalytics }} />
+        </head>
+        <body>
+          <ComposedComponent {...props} />
+        </body>
+      </html>
+    );
   };
 }
