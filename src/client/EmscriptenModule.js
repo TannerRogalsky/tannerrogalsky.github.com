f6a6696f34ea4b95ab5dd47a423e008e12acabce
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-12-02 10:06:45 -0500

    fix asset package path

--- src/client/EmscriptenModule.js
+++ src/client/EmscriptenModule.js
@@ -1,4 +1,5 @@
 const Module = { // eslint-disable-line no-unused-vars
+  filePackagePrefixURL: '/demoloops/',
   memoryInitializerPrefixURL: '/demoloops/',
   canvas: document.getElementById('canvas'),
 };
